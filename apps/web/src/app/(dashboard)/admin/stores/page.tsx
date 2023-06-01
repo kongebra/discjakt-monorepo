326f6c85b0055687bdbd344ce4eca3946df29eae
Svein Are Danielsen <svein.danielsen@inmeta.no>
2023-06-01 20:53:46 +0200

    refactor: move files into stores dir

--- apps/web/src/app/(dashboard)/admin/stores/page.tsx
+++ apps/web/src/app/(dashboard)/admin/stores/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { prisma } from "database";
-import StoresTableWrapper from "../../_components/StoresTableWrapper";
+import StoresTableWrapper from "./_components/StoresTableWrapper";
 
 export default async function Page() {
   const stores = await prisma.store.findMany({
